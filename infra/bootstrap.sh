0aae4a0844cc0630742e2720143965ec6a45c85a
vberube <vberube@proofpoint.com>
2025-07-21 23:51:14 -0700

    chore(infra): cleanup unused Terraform files, rename dynamo.db to dynamodb.tf, inline CoreDNS patch

--- infra/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# bootstrap.sh
-# Use locally only if DynamoDB table needs to be re-created
-mkdir terraform-bootstrap
-cd terraform-bootstrap
-terraform init
-terraform apply -auto-approve
-cd ..
-rm -rf terraform-bootstrap
